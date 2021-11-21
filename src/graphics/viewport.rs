dda82773294c26a7766efad0f372f70d035e7aa1
LyricWulf <lyricwulf@gmail.com>
2021-11-21 03:17:29 -0600

    fix buffer init: with_data_slice -> with_data

--- src/graphics/viewport.rs
+++ src/graphics/viewport.rs
@@ -84,7 +84,7 @@ impl<'a> Viewport {
 
         let data_buffer = gpu
             .create_buffer("Viewport buffer")
-            .with_data_slice(&[width as f32, height as f32])
+            .with_data(&[width as f32, height as f32])
             .as_uniform_buffer()
             .allow_copy_to()
             .build()
