17a250f98a978c859fff408c35c73bb5e125a027
LyricWulf <lyricwulf@gmail.com>
2021-12-14 16:00:29 -0600

    texture: add placeholder fns to use gpuhandle

--- src/graphics/texture.rs
+++ src/graphics/texture.rs
@@ -19,3 +19,28 @@ pub struct Texture {
     pub format: wgpu::TextureFormat,
 }
 crate::wgpu_inner_deref!(Texture);
+impl Texture {
+    pub fn new(gpu: GpuHandle, desc: &wgpu::TextureDescriptor) -> Self {
+        let inner = gpu.create_texture(desc);
+        let view = inner.create_view(&Default::default());
+        Self {
+            gpu,
+            inner,
+            view,
+            format: desc.format,
+        }
+    }
+
+    pub fn resize(&mut self, _size: &[u32]) {
+        self.gpu.create_texture(&wgpu::TextureDescriptor {
+            label: Default::default(),
+            size: Default::default(),
+            mip_level_count: Default::default(),
+            sample_count: Default::default(),
+            dimension: wgpu::TextureDimension::D2,
+            format: self.format,
+            usage: wgpu::TextureUsages::empty(),
+        });
+        todo!()
+    }
+}
