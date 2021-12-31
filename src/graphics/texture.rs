46e6fe11ecc210c90991bd1a123bc5e9b9a27f7f
LyricWulf <lyricwulf@gmail.com>
2021-12-31 13:10:56 -0600

    Texture::write() and write_at()

--- src/graphics/texture.rs
+++ src/graphics/texture.rs
@@ -75,4 +75,85 @@ impl Texture {
         self.size = new_size;
         self.usage = new_usage;
     }
+
+    pub fn write<T>(&self, data: &[T])
+    where
+        T: bytemuck::Pod,
+    {
+        self.write_at(0, data)
+    }
+
+    pub fn write_at<T>(&self, offset: wgpu::BufferAddress, data: &[T])
+    where
+        T: bytemuck::Pod,
+    {
+        let data_bytes = bytemuck::cast_slice::<_, u8>(data);
+        self.gpu.queue.write_texture(
+            self.inner.as_image_copy(),
+            data_bytes,
+            wgpu::ImageDataLayout {
+                offset,
+                bytes_per_row: std::num::NonZeroU32::new(data_bytes.len() as _),
+                rows_per_image: std::num::NonZeroU32::new(1),
+            },
+            wgpu::Extent3d {
+                width: data_bytes.len() as u32 / self.format.describe().block_size as u32,
+                height: 1,
+                depth_or_array_layers: 1,
+            },
+        )
+    }
+
+    // TODO
+    #[allow(unreachable_code)]
+    pub fn read_immediately(&self) -> Result<wgpu::util::DownloadBuffer, wgpu::BufferAsyncError> {
+        todo!("Texture::read_immediately :(");
+        let texel_count = self.size.width * self.size.height * self.size.depth_or_array_layers;
+        let size = texel_count * self.format.describe().block_size as u32;
+        let staging_buf = self
+            .gpu
+            .new_buffer("texture read staging buffer")
+            .allow_copy_to()
+            .allow_map_read()
+            .create_uninit(size as _);
+        let mut enc = self
+            .gpu
+            .create_command_encoder("texture read immediately enc");
+
+        let staging_copy = wgpu::ImageCopyBuffer {
+            buffer: &staging_buf,
+            layout: wgpu::ImageDataLayout {
+                offset: 0,
+                bytes_per_row: std::num::NonZeroU32::new(size),
+                rows_per_image: std::num::NonZeroU32::new(256),
+            },
+        };
+
+        enc.copy_texture_to_buffer(self.inner.as_image_copy(), staging_copy, self.size);
+
+        self.gpu.queue.submit([enc.finish()]);
+
+        staging_buf.download_immediately()
+    }
 }
+
+// TODO
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn texture_write() {
+//         let data = [10_u32, 20, 30];
+
+//         let gpu = crate::Gpu::builder().build_headless().unwrap();
+//         let texture = gpu
+//             .new_texture("resize test")
+//             .allow_copy_from()
+//             .create_empty(&[128, 128]);
+//         texture.write(&data);
+
+//         let texture_read = texture.read_immediately().unwrap();
+//         let texture_read = bytemuck::cast_slice::<_, u32>(&texture_read);
+
+//         assert_eq!(data, texture_read[..data.len()]);
+//     }
+// }
